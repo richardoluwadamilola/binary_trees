00ef48ed180f2d9067667c1190041b1a61e9c52d
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-12-16 09:41:34 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,7 +4,6 @@
  * binary_tree_balance - measure the balance factor
  * of a binary tree
  * @tree: pointer to root node of tree to measure
- *
  * Return: balance factor left height minus right height,
  * or 0 if tree is NULL
  */
@@ -26,7 +25,6 @@ int binary_tree_balance(const binary_tree_t *tree)
  * of a binary tree from a given node
  * @tree: pointer to node of tree to measure
  * Description: Edited to work with balance factor function
- *
  * Return: height of tree or -1 if NULL
  */
 int custom_binary_tree_height(const binary_tree_t *tree)
