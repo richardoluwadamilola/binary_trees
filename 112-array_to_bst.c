8230ff3e94bbab4221672f5bf815ccf7ff42f130
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-12-16 09:47:22 +0100

    fix

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,9 +1,28 @@
+
+Unique-Red
+/
+binary_trees
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+More
+binary_trees/112-array_to_bst.c
+@Unique-Red
+Unique-Red function that inserts a value in a Binary Search Tree
+ History
+ 1 contributor
+Executable File  23 lines (21 sloc)  428 Bytes
 #include "binary_trees.h"
 
 /**
  * array_to_bst - Creates a binary search tree from an array.
  * @array: The array of values.
  * @size: The length of the array.
+ *
  * Return: A pointer to the binary search tree, otherwise NULL.
  */
 bst_t *array_to_bst(int *array, size_t size)
