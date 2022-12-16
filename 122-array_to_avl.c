38430f9a7dd9ff74bd8ed747660440ba1984b7d2
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-12-16 09:56:15 +0100

    fix

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -4,6 +4,7 @@
  * array_to_avl - Creates an AVL tree from an array.
  * @array: The array of values.
  * @size: The length of the array.
+ *
  * Return: A pointer to the AVL tree, otherwise NULL.
  */
 avl_t *array_to_avl(int *array, size_t size)
